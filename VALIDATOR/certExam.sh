078fc43b41cd95a863fcaeac5f21be3ec2cc83ff
brownman <brownman556@gmail.com>
2014-09-10 15:15:56 +0300

    [ SERVICE ] +certExam

---
+++ VALIDATOR/certExam.sh
@@ -0,0 +1,14 @@
+
+
+
+
+
+run(){
+wmctrl -a certexam || ( thunar $HOME/Downloads/VCE )
+}
+
+run &
+
+#source /tmp/link
+#wmctrl -a Easy || ( $dir_root/SCRIPT/PROTOTYPES/BANK/sanity_loop/run.sh 300 )
+#$dir_root/SCRIPT/PROTOTYPES/BANK/window_switcher/run.sh
--- VALIDATOR/window_switcher.sh
+++
@@ -1,4 +0,0 @@
-
-source /tmp/link
-#wmctrl -a Easy || ( $dir_root/SCRIPT/PROTOTYPES/BANK/sanity_loop/run.sh 300 )
-$dir_root/SCRIPT/PROTOTYPES/BANK/window_switcher/run.sh
